4fc1bab9492e117ce3490b6349d5dc96d2515c57
suyichen <805788048@qq.com>
2019-02-26 10:21:37 +0800

    base

---
+++ app/src/main/java/com/syc/bluetoothdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.syc.bluetoothdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author suycihen
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
